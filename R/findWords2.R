da72128aa62e341ea22bbbc9c0ff5df023dc5514
bjw649 <bjw649@gmail.com>
2016-02-20 12:06:36 +1100

    commit fW2 comment

--- R/findWords2.R
+++ R/findWords2.R
@@ -1,8 +1,9 @@
 findWords2 <- function(fn = "tf.txt") {
   # read in the words from the file, into a vector of mode character
+  #Matloff p126 (Modified)
   pfn <- paste("../Data/", fn, sep = "")
   cat("pfn:  ",pfn)
-  txt <- scan(pfn,"")  
-  words <- split(1:length(txt), txt)     
+  txt <- scan(pfn,"")
+  words <- split(1:length(txt), txt)
   return(words)
 }
