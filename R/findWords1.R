7e59d4d7530867de63100d74abc84adce55f1ab8
bjw649 <bjw649@gmail.com>
2016-02-20 12:03:25 +1100

    commit 2

--- R/findWords1.R
+++ R/findWords1.R
@@ -1,14 +1,15 @@
 findWords1 <- function(fn = "tf.txt") {
   # read in the words from the file, into a vector of mode character
+  #Matloff p 92 (Modified)
   pfn <- paste("../Data/", fn, sep = "")
   cat("pfn:  ",pfn)
-  txt <- scan(pfn,"")  
+  txt <- scan(pfn,"")
   tl <- length(txt)
   cat("The text file was read and there are ", tl, " words.")
   wl <- list()      # Create a list called wl with no elements
   for (i in 1:tl) {
     wrd <- txt[i]  # i-th word in input file
     wl[[wrd]] <- c(wl[[wrd]],i)
-  }  
+  }
   return(wl)
 }
